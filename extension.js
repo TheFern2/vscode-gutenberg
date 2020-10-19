313bbf9b415499228475fadb9e057ae30c11bc99
Fernando B <fernandobe+git@protonmail.com>
2020-10-19 10:50:59 -0500

    bug-fixes

--- extension.js
+++ extension.js
@@ -19,11 +19,11 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-gutenberg.helloWorld', async function () {
+	let disposable = vscode.commands.registerCommand('vscode-gutenberg.printBook', async function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code!');
+		// vscode.window.showInformationMessage('Hello VS Code!');
 		const rootfolders = vscode.workspace.workspaceFolders
 		console.log(rootfolders)
 
@@ -46,30 +46,36 @@ function activate(context) {
 			rootPathUri = rootfolders[0].uri
 			rootPathFull = rootfolders[0].uri.path
 			console.log(`Root path found: ${rootfolders[0].name}`) // rootPathFull can also be an option
-		}
-		
-
-		const bookFolders = Promise.resolve(vscode.workspace.fs.readDirectory(rootPathUri))
-		let bookFilesPromise = []
-		
+		}		
+	
 		// ignore folders need to be part of configuration
 		const ignoreFolders = ['images']
-
-		bookFilesPromise = getBookFiles(bookFolders, rootPathFull, ignoreFolders, '.md')
 		
-		const bookFilesResponse = await Promise.resolve(bookFilesPromise)
-		console.log(bookFilesResponse)
+		const bookFoldersPromise = getBookFiles(rootPathFull, ignoreFolders, '.md')		
+		const bookFilesResponse = await Promise.resolve(bookFoldersPromise)
+		console.log(bookFilesResponse)		
 		
 		// Need to join the filesString for the pandoc command
 		let filesArray = []
 		let filesString = ''
 		bookFilesResponse.forEach(element => {
 			element.files.forEach(file => {
-				filesArray.push(`./${element.folderPath}/${file}`)
+				if(element.isFolder){
+					filesArray.push(`./${element.folderPath}/${file}`)
+				} else {
+					filesArray.push(`./${file}`)
+				}
+				
 			});
 		});
 
-		filesString = filesArray.join(" ")
+		if(filesArray.length > 0){
+			vscode.window.showInformationMessage('Files names have been collected');
+			filesString = filesArray.join(" ")
+		} else {
+			vscode.window.showErrorMessage('No files found!');
+			return
+		}		
 
 		// const lsCmdTest = `cd ${rootPathFull} && ls -la`
 		const pandocCmdTest = `cd ${rootPathFull} && pandoc -o book.pdf --pdf-engine=xelatex ${filesString}`
@@ -101,49 +107,89 @@ module.exports = {
 	deactivate
 }
 
-async function getBookFiles(foldersPromise, rootPath, ignoreFolders, fileExtension){
+async function getBookFiles(rootPath, ignoreFolders, fileExtension){
+
+	let bookFoldersPromise = await Promise.resolve(vscode.workspace.fs.readDirectory( vscode.Uri.file(rootPath)))
 	let bookFoldersPaths = []
-	let bookPaths = await Promise.resolve(foldersPromise)
+	let bookPaths = await Promise.resolve(bookFoldersPromise)
+	let bookFilesOnRoot = []
 
 	bookPaths.forEach(value => {
-		// only add folder paths
+		// only add folder paths here
 		if(value[1] == 2 && !ignoreFolders.includes(value[0])){
 			bookFoldersPaths.push(value[0])
-		}	
+		}
+		// add files on root, in case they need to be used later
+		if(value[1] == 1 && !ignoreFolders.includes(value[0])){
+			bookFilesOnRoot.push(value[0])
+		}
 	})
 
 	let bookFilesPromises = []
 	let bookFilesResponse = []
 	let bookFiles = []
 
-	bookFoldersPaths.forEach(folder => {
-		bookFilesPromises.push(vscode.workspace.fs.readDirectory(
-			vscode.Uri.file(path.join(rootPath, folder))
-		))
-	});
-
-	bookFilesResponse = await Promise.all(bookFilesPromises)
-
-	for (let i = 0; i < bookFoldersPaths.length; i++) {
-		// this should give us folder, plus files found in that folder
-		//bookFiles.push(bookFoldersPaths[i], bookFilesResponse[i])
-
+	if(bookFoldersPaths.length > 0){
+		bookFoldersPaths.forEach(folder => {
+			bookFilesPromises.push(vscode.workspace.fs.readDirectory(
+				vscode.Uri.file(path.join(rootPath, folder))
+			))
+		});
+	
+		bookFilesResponse = await Promise.all(bookFilesPromises)
+	
+		for (let i = 0; i < bookFoldersPaths.length; i++) {
+			// this should give us folder, plus files found in that folder
+			//bookFiles.push(bookFoldersPaths[i], bookFilesResponse[i])
+			const basePath = path.basename(bookFoldersPaths[i])
+			// only add files to tempFiles
+			const tempFiles = []
+			bookFilesResponse[i].forEach(element => {
+				if(element[1] == 1 && element[0].includes(fileExtension)){
+					tempFiles.push(element[0])
+				}
+				if(element[1] == 2){
+					vscode.window.showInformationMessage(`Directory ${basePath} should not have nested folders`);
+				}
+				if(element[1] == 1 && !element[0].includes(fileExtension)){
+					vscode.window.showInformationMessage(`Directory ${basePath} should only have ${fileExtension} files, ${element[0]} is not acceptable`);
+				}
+				//  else{
+				// 	// TODO error needs to be cleared on what to remove!
+				// 	vscode.window.showErrorMessage(`Directory ${basePath} should only have ${fileExtension} files, not folders or other file extensions`);
+				// }
+			});
+			
+			bookFiles.push({
+				"folderPath": bookFoldersPaths[i],
+				"files": tempFiles,
+				"isFolder": true
+			})	
+		}
+	} else {
+		// Do only files as no folder paths were found
 		// only add files to tempFiles
-		const tempFiles = []
-		bookFilesResponse[i].forEach(element => {
-			if(element[1] == 1 && element[0].includes(fileExtension)){
-				tempFiles.push(element[0])
+		
+		bookFilesOnRoot.forEach(element => {
+			const tempFiles = []
+			if(element.includes(fileExtension)){
+				tempFiles.push(element)
+				
 			} else{
 				// TODO error needs to be cleared on what to remove!
-				vscode.window.showErrorMessage(`Directories should only have ${fileExtension} files, not folders or other file extensions`);
+				vscode.window.showInformationMessage(`Files should only have ${fileExtension} extension, ${element} is not acceptable`);
 			}
-		});
+
+			if(tempFiles.length > 0){
+				bookFiles.push({
+					"folderPath": rootPath,
+					"files": tempFiles,
+					"isFolder": false
+				})
+			}
+		});			
 		
-		bookFiles.push({
-			"folderPath": bookFoldersPaths[i],
-			"files": tempFiles
-		})	
-	}
+	}	
 
 	return bookFiles
 }
---
+++ test/book2/Scene1.md
@@ -0,0 +1,42 @@
+---
+title: "Untitled"
+author: "Fernando ***REMOVED***"
+date: "10/13/20"
+header-includes:
+    - \usepackage{blindtext}
+    - \usepackage{fancyhdr}
+    - \pagestyle{fancy}
+    - \fancyhead[CO,CE]{This is fancy}
+    - \fancyfoot[CO,CE]{So is this}
+    - \fancyfoot[LE,RO]{\thepage}
+    - \usepackage{amsmath}
+---
+
+# Chapter 1 Scene 1
+
+Lorem ipsum dolor sit amet, **consectetur** adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. _Duis aute irure_ dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+>Lorem ipsum dolor sit amet, **consectetur** adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. _Duis aute irure_ dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+* First item
+* Second item
+* Third item
+
+---
+
+| Syntax | Description|
+|--------|------------|
+| Header | Title |
+| Paragraph | Text |
+
+## Blindtext
+
+\blindtext
+
+This formula $f(x) = x^2$ is an example.
+
+\begin{align*}
+  f(x) &= x^2\\
+  g(x) &= \frac{1}{x}\\
+  F(x) &= \int^a_b \frac{1}{3}x^3
+\end{align*}
